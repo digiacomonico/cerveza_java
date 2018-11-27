983726ee6581629bf894a79264cacc8787372d61
Nicolas Di Giacomo <nicolas.digiacomo@wenance.com>
2018-11-27 11:49:16 -0300

    Added Lager

--- src/main/java/model/Clasica.java
+++ src/main/java/model/Clasica.java
@@ -1,6 +1,27 @@
 package model;
 
-public class Clasica extends Cerveza{
+public class Clasica extends Cerveza {
 
+    protected double valorLevadura;
+
+    public double costo() {
+        return this.valorLupulo + this.valorLevadura;
+    }
+
+
+    public void tipoLevadura(String tipo) {
+        if (tipo == "Alta") {
+            this.valorLevadura = 0.1 * this.valorLupulo;
+        } else if (tipo == "Baja") {
+            this.valorLevadura = 0;
+        }
+    }
 
 }
+
+
+
+//    method valorDescuento(){
+//        return 0
+//    }
+
---
+++ src/main/java/model/Lager.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lager extends Clasica {
+
+    protected List<String> ingredientes = new ArrayList<String>();
+
+    
+
+
+//    var ingredientes = []
+//    var valorDescuento
+//
+//
+//    override method costo(){
+//        return (self.valorLupulo() + valorLevadura + (self.cantidadIngredientes() * 50)) //* (1 -valorDescuento)
+//    }
+//
+//    override method valorDescuento(){
+//        return valorDescuento
+//    }
+//
+//
+//
+//
+//    method agregarIngredientes(ingrediente){
+//        ingredientes.add(ingrediente)
+//    }
+//
+//    method quitarIngrediente(ingrediente){
+//        ingredientes.remove(ingrediente)
+//    }
+//
+//    method cantidadIngredientes(){
+//        if(ingredientes.size() == null){
+//            return 0
+//        }
+//        else return ingredientes.size()
+//    }
+//
+//}
+
+}
