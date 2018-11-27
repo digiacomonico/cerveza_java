46ee5cc58b580fd69ab74295539610113a7b3932
Nicolas Di Giacomo <nicolas.digiacomo@wenance.com>
2018-11-27 13:12:37 -0300

    added pedidos

--- src/main/java/model/Pedidos.java
+++ src/main/java/model/Pedidos.java
@@ -1,4 +1,59 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedidos {
+
+    protected List<Lotes> lotes = new ArrayList<Lotes>();
+    //protected int valorDescuento;
+    protected int distancia;
+
+    public void agregarLote(Lotes lote){
+        lotes.add(lote);
+    }
+
+    public int cantidadLotes(){
+        return lotes.size();
+    }
+
+    public void setDistancia(int kms){
+        this.distancia = kms;
+    }
+
+    public int getDistancia(){
+        return this.distancia;
+    }
+
+
+
+
+
+//    var lotes = []
+//    var valorDescuento = 0
+//    var distancia
+//
+//    method agregarLote(lote){
+//        lotes.add(lote)
+//    }
+//
+//    method quitarLote(lote){
+//        lotes.remove(lote)
+//    }
+//
+//    method cantidadLotes(){
+//        return lotes.size()
+//    }
+//
+//    method costo(){
+//        return (lotes.sum({l => l.costo()})) * (1 -valorDescuento)
+//    }
+//
+//    method distancia(){
+//        return distancia
+//    }
+//
+//    method distancia(kms){
+//        distancia = kms
+//    }
 }
